b04181c8dc8315bc2f9ff1eb2416568794075ad6
Joshua Beitler <joshbeitler@gmail.com>
2023-08-24 22:37:03 -0600

    Rules

--- src/pages/rules.js
+++ src/pages/rules.js
@@ -81,7 +81,7 @@ export default function Rules() {
               </p>
               <p className="flex flex-row">
                 <a href="#" className="mr-8">
-                  Venue directions
+                  Directions
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
@@ -95,6 +95,44 @@ export default function Rules() {
                     />
                   </svg>
                 </a>
+                <a href="#" className="mr-8">
+                  Calendar invite (Google)
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </p>
+            </Section>
+            <Section sectionTitle="Venue information" className="mb-20">
+              <p className="mb-4">
+                Our venue is in the gym of the Vasion building up on Tech Ridge.
+                This is the same venue we have used for the last several years.
+                It fits several hundred people comfortably, and has plenty of
+                space for everyone to spread out. There is a loft space inside
+                the gym with couches for more seating (and sleeping) options.
+              </p>
+              <p className="mb-4">
+                We will be able to open the large bay doors in the gym to let in
+                fresh air as well as facilitate loading and unloading. We will
+                mark off the street next to them specifically for loading and
+                unloading. Please do not park in this area.
+              </p>
+              <p className="mb-4">
+                Please remember our venue is also an active workplace. Please do
+                not disturb the employees of Vasion or any other tenants in the
+                building during the event, especially on Friday while they are
+                still in office. Do not enter any areas of the building that are
+                not part of the event — they will be clearly marked so there are
+                no excuses.
               </p>
             </Section>
             {/* <Section sectionTitle="Registration" className="mb-20">
@@ -122,9 +160,26 @@ export default function Rules() {
                 parent or guardian sign a permission slip and you must be
                 accompanied by a responsible adult at all times.
               </p>
-              <p>
+              <p className="mb-4">
                 We reserve the right to remove anyone violating these rules from
                 the event at any time.
+              </p>
+              <p className="flex flex-row">
+                <a href="#" className="mr-8">
+                  Code of Conduct
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
               </p>
             </Section>
             <Section sectionTitle="Under 18?" className="mb-20">
@@ -188,6 +243,30 @@ export default function Rules() {
                 We highly recommend you attend the Preflight Meeting, but it is
                 not required. Pizza and drinks are provided.
               </p>
+              <p className="mb-4">
+                This year's Preflight Meeting is being held on November XXth at
+                7:00pm in the Vasion gym — the same venue as the main event.
+              </p>
+              <p className="flex flex-row">
+                <a href="#" className="mr-8">
+                  Calendar invite (Google)
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    className="w-5 h-5"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </a>
+              </p>
+            </Section>
+            <Section sectionTitle="AI tools" className="mb-20">
+              <p className="mb-4">ASDF</p>
             </Section>
             <Section sectionTitle="Open source & 3rd party" className="mb-20">
               <p className="mb-4">
@@ -222,6 +301,83 @@ export default function Rules() {
                 encourage contestants to open source their code, but it’s
                 totally up to you. If you want to start a business based on it,
                 awesome.
+              </p>
+            </Section>
+            <Section sectionTitle="Team prep" className="mb-20">
+              <p className="mb-4">
+                We encourage you to come prepared with a team and a project
+                idea. You are more than welcome to spend time preparing for the
+                event doing activites such as planning, designing, and
+                researching. However, you are not allowed to write any code for
+                your project before the event begins.
+              </p>
+              <p className="mb-4">
+                If you are looking for a team or a project idea, we will try to
+                help you out at the Preflight Meeting. During the main event,
+                our staff and volunteers will be available to help you find a
+                team and/or a project idea but please keep in mind that they are
+                not there to do the work for you.
+              </p>
+            </Section>
+            <Section sectionTitle="Computers and tools" className="mb-20">
+              <p className="mb-4">
+                It is your responsibility to bring your own computer and any
+                other tools you need to work on your project. This includes
+                screens, 3D printers, extra chargers, soldering irons, etc. We
+                will provide power and networking, see below for details.
+              </p>
+              <p className="mb-4">
+                We will provide tables and chairs for you to work at. Be aware
+                the chairs are not that comfortable. You are more than welcome
+                to bring your own chair.
+              </p>
+            </Section>
+            <Section sectionTitle="Networking and power" className="mb-20">
+              <p className="mb-4">
+                We will provide power to each table in the venue as well as
+                speedy Wifi. We have a small amount of ethernet cables available
+                on some of the tables - if you need a wired connection, please
+                bring your own cables and switches.
+              </p>
+              <p className="mb-4">
+                If you have any special networking and hardware needs that are
+                required for your project (such as extra space for 3D printers,
+                etc.), please let us know when you register and we will do our
+                best to accommodate.
+              </p>
+            </Section>
+            <Section sectionTitle="Snacks and food" className="mb-20">
+              <p className="mb-4">
+                We will provide snacks and drinks throughout the event. We also
+                provide breakfast, lunch, and dinner on Friday, and breakfast on
+                Saturday. If you have any special dietary needs, please let us
+                know when you register and we will do our best to accommodate.
+              </p>
+            </Section>
+            <Section sectionTitle="Noise and sensory" className="mb-20">
+              <p className="mb-4">
+                CodeCamp is a busy, noisy, and crowded event. If you have any
+                concerns with this we encourage you to bring noise-cancelling
+                headphones, earplugs, or any other tools you need to make
+                yourself comfortable.
+              </p>
+            </Section>
+            <Section sectionTitle="Cleanliness" className="mb-20">
+              <p className="mb-4">
+                We expect everyone to clean up after themselves. We will provide
+                multiple easy to find trash cans throughout the venue. Please
+                use them. Vasion has been extremely kind to allow us to use
+                their gym for the last several years and we want to make sure we
+                leave it in better condition than we found it.
+              </p>
+              <p className="mb-4">
+                While we provide food, snacks, and drinks throughout the event,
+                please keep your workspace clean and tidy.
+              </p>
+              <p className="mb-4">
+                Please keep the bathrooms clean and tidy. We will make sure they
+                are stocked with toilet paper and paper towels. If you notice
+                they are running low, please let us know.
               </p>
             </Section>
           </div>
