d941eb9163fd67f1cc253ca76f3f9146e120f35e
Joshua Beitler <oss@joshbeitler.com>
2023-08-29 17:05:33 -0600

    Spacing

--- src/components/section.js
+++ src/components/section.js
@@ -27,7 +27,7 @@ export const Section = ({ sectionTitle, children, className = "" }) => {
       className={`${className} mx-auto ring-1 ring-zinc-700/50 border-solid p-20 rounded-3xl bg-zinc-800/30 hover:ring-zinc-700 transition-all ease-in-out hover:bg-zinc-700/20 hover:shadow-xl`}
     >
       <a id={anchorId}>
-        <h2 className="flex-1 font-medium text-white mb-4 text-3xl relative group flex flex-row items-center">
+        <h2 className="flex-1 font-medium text-white mb-8 text-3xl relative group flex flex-row items-center">
           {sectionTitle}
           <svg
             onClick={handleCopyClick}
