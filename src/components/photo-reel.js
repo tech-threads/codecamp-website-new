36c370a6368e53d4b36f9a5b131436aab138e4c5
Joshua Beitler <oss@joshbeitler.com>
2023-09-13 16:21:56 -0600

    Use photo reel images

--- src/components/photo-reel.js
+++ src/components/photo-reel.js
@@ -1,48 +1,43 @@
 import Image from "next/image";
 
+const PhotoReelImage = ({ rotation, imageSrc, altText }) => (
+  <div
+    className={`transition ease-in-out duration-300 shadow-xl relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2x ${rotation}`}
+  >
+    <Image
+      src={imageSrc}
+      alt={altText}
+      fill
+      style={{
+        objectFit: "cover",
+      }}
+    />
+  </div>
+);
+
 export const PhotoReel = ({ className }) => (
   <div
     className={`-my-4 -mx-40 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8 ${className}`}
   >
-    <div className="shadow-xl relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl rotate-2">
-      <Image
-        src="/photo1.jpeg"
-        alt="Photo"
-        fill
-        style={{
-          objectFit: "cover",
-        }}
-      />
-    </div>
-    <div className="shadow-xl relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl -rotate-2">
-      <Image
-        src="/photo2.jpeg"
-        alt="Photo"
-        fill
-        style={{
-          objectFit: "cover",
-        }}
-      />
-    </div>
-    <div className="shadow-xl relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl rotate-2">
-      <Image
-        src="/photo3.jpeg"
-        alt="Photo"
-        fill
-        style={{
-          objectFit: "cover",
-        }}
-      />
-    </div>
-    <div className="shadow-xl relative aspect-[9/10] w-44 flex-none overflow-hidden rounded-xl bg-zinc-100 dark:bg-zinc-800 sm:w-72 sm:rounded-2xl -rotate-1">
-      <Image
-        src="/photo4.jpeg"
-        alt="Photo"
-        fill
-        style={{
-          objectFit: "cover",
-        }}
-      />
-    </div>
+    <PhotoReelImage
+      rotation="rotate-2"
+      imageSrc="/photo1.jpeg"
+      altText="Photo"
+    />
+    <PhotoReelImage
+      rotation="-rotate-2"
+      imageSrc="/photo2.jpeg"
+      altText="Photo"
+    />
+    <PhotoReelImage
+      rotation="rotate-2"
+      imageSrc="/photo3.jpeg"
+      altText="Photo"
+    />
+    <PhotoReelImage
+      rotation="-rotate-1"
+      imageSrc="/photo4.jpeg"
+      altText="Photo"
+    />
   </div>
 );
